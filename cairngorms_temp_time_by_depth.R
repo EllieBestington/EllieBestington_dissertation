66ce410b80fc599357920ae7eca484afde005403
EllieBestington <s2277781@ed.ac.uk>
2025-08-27 18:35:22 +0100

    new figure script

---
+++ cairngorms_temp_time_by_depth.R
@@ -0,0 +1,29 @@
+# MOCK FIGURE CREATION 
+# CAIRNGORMS TEMP OVER TIME BY DEPTH?
+# 27/08/2025
+# Author: Ellie Bestington
+
+# Libraries----
+library(tidyverse)
+library(ggplot2)
+
+# FIGURE----
+
+# With depth split up by facet wrap
+library(ggplot2)
+
+(ggplot(combined_data) +
+ aes(x = date_time, y = temperature, colour = type, group = type) +
+ geom_line() +
+ theme_minimal() + 
+ facet_wrap(~depth)
+)
+
+# Combined 
+(ggplot(combined_data) +
+    aes(x = date_time, y = temperature, fill = depth, colour = type, group = type) +
+    geom_col() +
+    scale_fill_gradient() +
+    scale_color_hue(direction = 1) +
+    theme_minimal() 
+)
