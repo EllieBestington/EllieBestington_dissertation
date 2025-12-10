1003bba2f89681126322094db93fa1c8ea6dff44
EllieBestington <s2277781@ed.ac.uk>
2025-12-10 10:09:45 +0000

    update colours and extra figure option

--- scripts/RQ1/community_biomass_year.R
+++ scripts/RQ1/community_biomass_year.R
@@ -24,3 +24,16 @@ QHI_combined_data <- read_excel("datasets/QHI_roots_data/QHI_combined_data.xlsx"
   scale_fill_manual(values = c("2023" = "deepskyblue3", "2024" = "brown3"))+
   theme_classic() 
 )
+
+# FIGURE 2 IDEA----
+(year_biomass_community<-QHI_combined_data %>%
+    filter(year %in% c(2023, 2024)) %>%
+    ggplot(aes(x = as.factor(year), y = root_dry_biomass_total, fill = community)) +
+    geom_boxplot(position = position_dodge(width = 0.8), alpha = 0.7) +
+    labs(
+      x = "Year",
+      y = "Dry Root Biomass (g)",
+      fill = "Community") +
+    scale_fill_manual(values = c("Graminoid" = "#E69F00", "Shrub" = "#009E73", "Mix"="#CC79A7"))+
+    theme_classic() 
+)
