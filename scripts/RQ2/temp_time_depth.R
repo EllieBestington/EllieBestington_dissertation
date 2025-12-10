628dc3518cb9fa12d8d7723411e219e9bdbc1a83
EllieBestington <s2277781@ed.ac.uk>
2025-12-10 16:42:42 +0000

    Adding extra figure of just surface temp

--- scripts/RQ2/temp_time_depth.R
+++ scripts/RQ2/temp_time_depth.R
@@ -54,3 +54,25 @@ clean_control_exp_data <- read_excel("datasets/lab_experiment/clean_control_exp_
       legend.text = element_text(size = 10)
     )
 )
+
+# FIGURE JUST SURFACE TEMP ----
+(surface_temp_time<-clean_control_exp_data %>%
+   filter(depth == 0) %>%
+   ggplot(aes(x = hour, y = temp, color = type, group = type)) +
+   geom_line() +
+   geom_point(size = 2) +
+   labs(
+     x = "Time (hours)",
+     y = "Surface Temperature (°C)",
+     color = "Treatment"
+   ) +
+   scale_color_manual(labels = c("con"="Control", "exp"= "Experimental") ,values = c("con" = "deepskyblue2", "exp" = "brown1"))+
+   theme_classic() +
+   theme(
+     plot.title = element_text(hjust = 0.5, size = 16, face = "bold"),
+     axis.title.x = element_text(size = 14),
+     axis.title.y = element_text(size = 14),
+     legend.title = element_text(size = 12),
+     legend.text = element_text(size = 10)
+   )
+)
