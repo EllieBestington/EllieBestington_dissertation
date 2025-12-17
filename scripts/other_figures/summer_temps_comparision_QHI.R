5cf5c6212fcc1ecb65c2b12c578560ef43c8ccf2
EllieBestington <s2277781@ed.ac.uk>
2025-12-17 10:21:51 +0000

    summer temperature trends at QHI 2023 and 2024 comparison

---
+++ scripts/other_figures/summer_temps_comparision_QHI.R
@@ -0,0 +1,30 @@
+# RQ0 
+# TEMPERATURE TRENDS BETWEEN SUMMERS AT QHI 
+# I.E. HEATWAVES OCCURING IN 2024 
+# 12/12/2025
+
+# LOAD LIBRARIES----
+library(tidyverse)
+library(readxl)
+library(ggplot2)
+library(dplyr)
+
+# LOAD DATA----
+QHI_climate_combined <- read_excel("dissertation_2026_bestington/datasets/climate_data/QHI_climate_combined.xlsx")
+
+# FIGURE----
+(QHI_climate_combined %>%
+   filter(year %in% c(2023, 2024)) %>%
+   mutate(year = factor(year)) %>%  # Convert year to factor for proper grouping
+   ggplot(aes(x = date_time, y = temp, color = year, group = year)) +
+   geom_line(linewidth = 0.5, alpha= 0.4) +  # Changed from 'size' to 'linewidth'
+   geom_point(alpha = 0.3) +
+   geom_hline(yintercept = 26, linetype = "dashed", color = "black", linewidth = 0.8) +
+   scale_color_manual(values = c("2023" = "deepskyblue3", "2024" = "brown3")) +  # Changed from scale_fill_manual
+   labs(title = "Summer Temperature Trends at QHI (2023-2024)",
+        x = "Time",
+        y = "Surface Temperature (°C)",
+        color = "Year") +  # Added legend title
+   theme_classic() +
+   theme(legend.position = "top")
+)
