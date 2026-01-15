c07b1ae578100a688ecf3e9e9356a843aad87196
EllieBestington <s2277781@ed.ac.uk>
2026-01-15 16:59:29 +0000

    adding analysis

--- scripts/data_analysis/rq1_1.R
+++ scripts/data_analysis/rq1_1.R
@@ -1,4 +1,4 @@
-# RQ1 ANALYSIS PART 1 
+# RQ1 ANALYSIS PART 1 : SIGNIFICANT DIFFERENCE BETWEEN 2023 AND 2024 ROOT BIOMASS?
 # 14/01/2026 
 
 # LOAD LIBRARIES -----
@@ -21,21 +21,43 @@ ggplot(QHI_combined_data, aes(x = root_dry_biomass_total, fill= year)) +
        y = "Count") +
   theme_minimal()
 
-# log transform root biomass data
-QHI_combined_data <- QHI_combined_data %>%
-  mutate(log_root_dry_biomass_total = log(root_dry_biomass_total + 1)) # add 1 to avoid log(0)
-# VISUALISE LOG TRANSFORMED DATA WITH HISTOGRAM----
-ggplot(QHI_combined_data, aes(x = log_root_dry_biomass_total , fill= year)) +
-  geom_histogram(binwidth = 0.05, position = "dodge", color = "black") +
-  labs(title = "Distribution of Log Transformed Total Dry Root Biomass by Year ",
-       x = "Log Transformed Total Root Biomass",
-       y = "Count") +
-  theme_minimal() 
-# still not normal - non parametric tests required?
+# VISUALISING MEANS WITH BOXPLOT----
+(root_boxplot<-ggplot(QHI_combined_data, aes(x = year, y = root_dry_biomass_total, fill = year)) +
+    geom_boxplot(alpha = 0.7) +
+    labs(title = "Boxplot of Total Dry Root Biomass by Year",
+         x = "Year",
+         y = "Total Root Biomass") +
+    scale_fill_manual(values = c("2023" = "deepskyblue3", "2024" = "brown3")) +
+    theme_classic()+
+   theme(legend.position = "none")
+)
+# likely not significant as overlap of boxes and whiskers but let's check anyway
+
+# SIMPLE ANOVA----
+roots_anova <- aov(root_dry_biomass_total ~ year, data = QHI_combined_data)
+summary(roots_anova)
+
+# p= 0.478, no significant difference in overall root biomass between years 
+
+# TRY AS LINEAR MODEL ----
+roots_lm <- lm(root_dry_biomass_total ~ year, data = QHI_combined_data)
+summary(roots_lm)
+
+# CHECKING ASSUMPTIONS ----
+# checking normality
+hist(roots_anova$residuals)
+plot(roots_anova, which = 2)
+shapiro.test(roots_anova$residuals)
+# not normally distributed 
+
+# checking homoscedasticity
+plot(roots_anova, which = 1)
+bartlett.test(root_dry_biomass_total ~ year, data = QHI_combined_data)
+
 
 # NON-PARAMETRIC TESTS?? -----
-# Kruskal-Wallis test to compare root biomass across years
-kruskal.test(root_dry_biomass_total ~ year, data = QHI_combined_data) 
+# Wilcox Test 
+wilcox.test(root_dry_biomass_total ~ year, data = QHI_combined_data)
+# p=0.06195 -> no statistically significant difference 
 
 
-pairwise.wilcox.test
