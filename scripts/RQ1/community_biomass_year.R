745f521f774c9147e47e23e06ba544c6abd93ca8
EllieBestington <s2277781@ed.ac.uk>
2026-01-22 13:56:06 +0000

    cleaning data and changing to root biomass density

--- scripts/RQ1/community_biomass_year.R
+++ scripts/RQ1/community_biomass_year.R
@@ -9,33 +9,46 @@ library(ggplot2)
 library(readxl)
 library(lme4)
 
+setwd("C:/Users/ellie/OneDrive/Documents/dissertation_2026_bestington")
+
 # LOAD DATA----
 QHI_combined_data <- read_excel("datasets/QHI_roots_data/QHI_combined_data.xlsx")
 
 
+# CLEAN DATA----
+QHI_combined_data <- QHI_combined_data %>%
+  mutate(rootmass_bulkdensity = as.numeric(rootmass_bulkdensity)) %>%
+  filter(!is.na(rootmass_bulkdensity)) %>%
+  # remove row 32
+  filter(row_number() != 32)
+  
+# removed outlier row 32 which had rootmass_bulkdensity of 5.98 g/cm3, likely a data entry error 
+# see notebook for further laboratory explanation 
+
 # FIGURE -----
-(community_biomass_year<-QHI_combined_data %>%
+(community_biomass_year <- QHI_combined_data %>%
   filter(year %in% c(2023, 2024)) %>%
-  ggplot(aes(x = community, y = root_dry_biomass_total, fill = as.factor(year))) +
+  ggplot(aes(x = community, y = rootmass_bulkdensity, fill = as.factor(year))) +
   geom_boxplot(position = position_dodge(width = 0.8), alpha = 0.7) +
   labs(
-       x = "Community",
-       y = "Dry Root Biomass (g)",
-       fill = "Year") +
-  scale_fill_manual(values = c("2023" = "deepskyblue3", "2024" = "brown3"))+
-  theme_classic() 
+    x = "Community",
+    y = "Root Biomass Density (g cm⁻³)",
+    fill = "Year") +
+  scale_fill_manual(values = c("2023" = "deepskyblue3", "2024" = "brown3")) +
+  scale_y_continuous(labels = scales::label_number(accuracy = 0.1)) +
+  theme_classic()
 )
-
 
 # FIGURE 2 IDEA----
 (year_biomass_community<-QHI_combined_data %>%
     filter(year %in% c(2023, 2024)) %>%
-    ggplot(aes(x = as.factor(year), y = root_dry_biomass_total, fill = community)) +
+    ggplot(aes(x = as.factor(year), y = rootmass_bulkdensity, fill = community)) +
     geom_boxplot(position = position_dodge(width = 0.8), alpha = 0.7) +
     labs(
       x = "Year",
-      y = "Dry Root Biomass (g)",
+      y = "Root Biomass Density (g cm⁻³)",
       fill = "Community") +
     scale_fill_manual(values = c("Graminoid" = "#E69F00", "Shrub" = "#009E73", "Mix"="#CC79A7"))+
+    scale_y_continuous(labels = scales::label_number(accuracy = 0.1)) +
     theme_classic() 
 )
