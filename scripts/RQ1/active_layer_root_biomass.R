1c939aa867d0261a6ee89f415a38d8e6f6cfc098
EllieBestington <s2277781@ed.ac.uk>
2026-01-22 14:10:26 +0000

    changing to root biomass density

--- scripts/RQ1/active_layer_root_biomass.R
+++ scripts/RQ1/active_layer_root_biomass.R
@@ -10,6 +10,19 @@ library(readxl)
 ald_data <- read_excel("datasets/QHI_roots_data/active_layer_depths_data.xlsx")
 QHI_root_data <- read_excel("datasets/QHI_roots_data/QHI_combined_data.xlsx")
 
+# note: active layer depths not taken from same place as root biomass samples- working on trying to get data on this 
+
+# CLEAN DATA----
+QHI_root_data <- QHI_root_data %>%
+  mutate(rootmass_bulkdensity = as.numeric(rootmass_bulkdensity)) %>%
+  filter(!is.na(rootmass_bulkdensity)) %>%
+  # remove row 32
+  filter(row_number() != 32)
+
+# removed outlier row 32 which had rootmass_bulkdensity of 5.98 g/cm3, likely a data entry error 
+# see notebook for further laboratory explanation 
+
+
 # NEW DATAFRAME FOR PLOTTING----
 # Create the new combined dataframe
 
@@ -18,7 +31,7 @@ QHI_root_data <- read_excel("datasets/QHI_roots_data/QHI_combined_data.xlsx")
 # Here's an alternative if site and subplot should match:
 
 merged_data <- merge(
-  QHI_root_data[, c("year", "subplot", "max_depth_increment", "community", "root_dry_biomass_total")],
+  QHI_root_data[, c("year", "subplot", "max_depth_increment", "community", "rootmass_bulkdensity")],
   ald_data[, c("year", "site", "depth")],
   by.x = c("year", "subplot"),
   by.y = c("year", "site"),
@@ -27,7 +40,7 @@ merged_data <- merge(
 
 # Select and rename columns to match your requirements
 merged_data <- merged_data[, c("year", "subplot", "max_depth_increment", 
-                                   "community", "root_dry_biomass_total", "depth")]
+                                   "community", "rootmass_bulkdensity", "depth")]
 colnames(merged_data)[2] <- "site"
 colnames(merged_data)[6] <- "active_layer_depth"
 
@@ -36,38 +49,40 @@ head(merged_data)
 
 # FIGURE 1----
 # Scatter plot of Active Layer Depth vs Root Biomass by Year
-ggplot(merged_data, aes(x = active_layer_depth, y = root_dry_biomass_total, color = as.factor(year))) +
+ggplot(merged_data, aes(x = active_layer_depth, y = as.numeric(rootmass_bulkdensity), color = as.factor(year))) +
   geom_point(alpha=0.5) +
   geom_smooth(method = "lm", se = FALSE) +
   labs(
     title = "Active Layer Depth vs Root Biomass by Year",
     x = "Active Layer Depth (cm)",
-    y = "Root Dry Biomass Total (g)",
+    y = "Root Biomass Density (g cm⁻³)",
     color = "Year"
   ) +
   scale_colour_manual(values = c("2023" = "deepskyblue3", "2024" = "brown3"))+
   theme_classic()
 
+
+
 # FIGURE 2----
 # Scatter plot of Active Layer Depth vs Root Biomass by Community Type
-ggplot(merged_data, aes(x = active_layer_depth, y = root_dry_biomass_total, color = community)) +
+ggplot(merged_data, aes(x = active_layer_depth, y = rootmass_bulkdensity, color = community)) +
   geom_point(alpha= 0.5) +
   geom_smooth(method = "lm", se = FALSE) +
   labs( title = "Active Layer Depth vs Root Biomass by Community Type",
     x = "Active Layer Depth (cm)",
-    y = "Root Dry Biomass Total (g)",
+    y = "Root Biomass Density (g cm⁻³))",
     color = "Community Type")+
   scale_colour_manual(values = c("Graminoid" = "#E69F00", "Shrub" = "#009E73", "Mix"="#CC79A7"))+
   theme_classic()
 
 # FIGURE 3----
 # Scatter plot of Active Layer Depth vs Root Biomass by Year and Community Type AS FACET WRAP
-ggplot(merged_data, aes(x = active_layer_depth, y = root_dry_biomass_total, color = community)) +
+ggplot(merged_data, aes(x = active_layer_depth, y = rootmass_bulkdensity, color = community)) +
   geom_point(alpha= 0.5) +
   geom_smooth(method = "lm", se = FALSE) +
   labs( title = "Active Layer Depth vs Root Biomass by Year and Community Type",
         x = "Active Layer Depth (cm)",
-        y = "Root Dry Biomass Total (g)",
+        y = "Root Biomass Density (g cm⁻³)",
         color = "Community Type")+
   scale_colour_manual(values = c("Graminoid" = "#E69F00", "Shrub" = "#009E73", "Mix"="#CC79A7"))+
   theme_classic()+
