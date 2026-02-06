8841ec4b5eae8cc32e87c6d0d1f13c1dd46f772d
EllieBestington <s2277781@ed.ac.uk>
2026-02-06 12:39:42 +0000

    adding 2024 data

--- scripts/RQ2/slope_elevations_calibration_ald.R
+++ scripts/RQ2/slope_elevations_calibration_ald.R
@@ -20,6 +20,7 @@ proj4string(ald_only) <- CRS("+proj=longlat +datum=WGS84")
 
 # Transform to UTM Zone 7N to match your raster
 sites_utm <- spTransform(ald_only, crs(elevation))
+sites_utm <- spTransform(ald_only, crs(slope))
 
 
 
@@ -77,8 +78,7 @@ slope_values_df
 # COMBINE ELEVATION AND SLOPE DATAFRAMES----
 ald_elevation_slope_coordinates_ <- read_excel("~/dissertation_2026_bestington/datasets/QHI_roots_data/ald_elevation_slope_coordinates .xlsx")
 
-
-# PLOT RELATIONSHIPS----
+# PLOT RELATIONSHIPS DAY 228----
 # filter day 228 (last day of measurments )
 day228 <- ald_elevation_slope_coordinates_[ald_elevation_slope_coordinates_$doy == 228, ]
 
@@ -99,3 +99,38 @@ ggplot(day228, aes(x = slope, y = thaw_av)) +
        x = "Slope (degrees)",
        y = "Thaw Depth (cm)") +
   theme_classic()
+
+
+# PLOT RELATIONSHIPS ALL DAYS----
+# plot thaw against slope for two years
+ggplot(ald_elevation_slope_coordinates_, aes(x = slope, y = thaw_av, color = as.factor(year))) +
+  geom_point(size = 3) +
+  geom_smooth(method = "lm", se = FALSE) +
+  labs(title = "Active Layer Thaw Depth vs Slope Across Days",
+       x = "Slope (degrees)",
+       y = "Thaw Depth (cm)",
+       color = "Year") +
+  theme_classic()
+
+# filter to last day of data for each site group each year
+ald_elevation_slope_coordinates_last_day <- ald_elevation_slope_coordinates_ %>%
+  group_by(year, site) %>%
+  filter(doy == max(doy))
+
+# plot thaw against slope for last day
+ggplot(ald_elevation_slope_coordinates_last_day, aes(x = slope, y = thaw_av, color = as.factor(year))) +
+  geom_point(size = 3) +
+  geom_smooth(method = "lm", se = FALSE) +
+  labs(title = "Active Layer Thaw Depth vs Slope on Last Day of Measurements",
+       x = "Slope (degrees)",
+       y = "Thaw Depth (cm)",
+       color = "Year") +
+  theme_classic()
+
+# While we observed slightly deeper thaw on gentle slopes, the dramatic 2024 decline occurred across all slope positions
+# suggesting the cold winter/late snow melt affected the entire landscape regardless of topography??
+
+# pattern of steeper = deeper is interesting though
+# could be due to steeper slopes having better drainage and thus less insulating water content in the soil, leading to deeper thaw (drier soil conduct heat better)
+# but we also need to acknowledge that slope degree only to 7 degrees, so we are not talking about very steep slopes here, and the relationship is not super strong, so this is just a hypothesis to explore further with more data and analysis
+# ideally would need steeper slopes but this puts researchers in field at risk especially with active layer detatchments occuring 
