52f3ab5417de3110f8012ed31e6f1915c253730b
EllieBestington <s2277781@ed.ac.uk>
2026-03-02 12:27:16 +0000

    create script for data analysis of fig lab exp

---
+++ scripts/data_analysis/lab_exp.R
@@ -0,0 +1,80 @@
+# RQ2: LAB EXPERIMENT DATA ANALYSIS 
+# Is there a significant difference between changes in surface temp and changes in 30 cm depth across time?
+
+# LOAD LIBRARIES----
+library(tidyverse)
+library(ggplot2)
+library(readxl)
+library(forecast)
+library(colortools)
+
+# LOAD DATA----
+clean_control_exp_data <- read_excel("datasets/lab_experiment/clean_control_exp_data.xlsx")
+
+
+# VISUALISATION ----
+(change_temp_time <- clean_control_exp_data %>%
+   filter(!is.na(change_in_temp), !is.na(hour)) %>%
+   filter(hour >= 24) %>% # filter to remove first day as initial thawing period where temp changes are due limited thawing from control
+   ggplot(aes(x = hour, y = change_in_temp, color = depth, group = depth)) +
+   geom_line() +
+   geom_point(size = 2) +
+   scale_color_gradient(high = "#132B43", low = "#56B1F7") +  # dark to light
+   labs(
+     x = "Time (hours)",
+     y = "Difference in Temperature (°C)",
+     color = "Depth (cm)"
+   ) +
+   theme_classic() +
+   theme(
+     plot.title = element_text(hjust = 0.5, size = 16, face = "bold"),
+     axis.title.x = element_text(size = 14),
+     axis.title.y = element_text(size = 14),
+     legend.title = element_text(size = 12),
+     legend.text = element_text(size = 10)
+   )
+)
+
+# plot smooth line to plot trends for each depth
+(change_temp_time_smooth <- clean_control_exp_data %>%
+   filter(!is.na(change_in_temp), !is.na(hour)) %>%
+   filter(hour >= 24) %>% # filter to remove first day as initial thawing period where temp changes are due limited thawing from control
+   ggplot(aes(x = hour, y = change_in_temp, color = depth, group = depth)) +
+   geom_smooth(method = "loess", se = FALSE) +
+   scale_color_gradient(high = "#132B43", low = "#56B1F7") +  # dark to light
+   labs(
+     x = "Time (hours)",
+     y = "Difference in Temperature (°C)",
+     color = "Depth (cm)"
+   ) +
+   theme_classic() +
+   theme(
+     plot.title = element_text(hjust = 0.5, size = 16, face = "bold"),
+     axis.title.x = element_text(size = 14),
+     axis.title.y = element_text(size = 14),
+     legend.title = element_text(size = 12),
+     legend.text = element_text(size = 10)
+   )
+)
+
+# ANALYSIS----
+# Fit a linear model to test for significant differences in temperature change across depths and time
+lm_model <- lm(change_in_temp ~ hour * depth, data = clean_control_exp_data)
+summary(lm_model)
+
+# check model assumptions
+shapiro.test(residuals(lm_model)) # check normality of residuals
+hist(residuals(lm_model)) # visualize residuals
+
+# log transform the response variable to meet normality assumption
+clean_control_exp_data <- clean_control_exp_data %>%
+  mutate(log_change_in_temp = log(change_in_temp + abs(min(change_in_temp, na.rm = TRUE)) + 1)) # log transform with shift to handle negative values
+
+lm_model_log <- lm(log_change_in_temp ~ hour * depth, data = clean_control_exp_data)
+summary(lm_model_log)
+
+shapiro.test(residuals(lm_model_log)) # check normality of residuals after log transformation
+hist(residuals(lm_model_log)) # visualize residuals after log transformation
+# better 
+
+
